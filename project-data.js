ef93859038167fdd3722b0390a64d9ecee195da5
Chetan Shrivas <chetanshrivas1234@gmail.com>
2025-07-24 01:33:11 +0530

    Initial clean commit

---
+++ project-data.js
@@ -0,0 +1,117 @@
+const projectData = [
+  {
+    title: "Tomato",
+    miniDesc:
+      "Tomato is a MERN-stack food-ordering platform featuring secure JWT authentication, real-time inventory, seamless CRUD, and integrated online payments.",
+    website: "https://tomato-olive-three.vercel.app/",
+    github: "https://github.com/Chetanshrivas/Tomato-back-end",
+    img: "/assets/images/projects/Tomato.webp.png",
+    seoimg: "/assets/images/projects/Tomato.webp.png",
+    link: "food-store",
+    showOnHome: true,
+    tech: [
+      "JavaScript",
+      "React,js",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "Bootstrap",
+      "JWT",
+      "Cloudinary"
+    ],
+    desc: "Tomato is a MERN-stack food-ordering platform featuring secure JWT authentication, real-time inventory, seamless CRUD, and integrated online payments."
+  },
+  {
+    title: "Shopping cart (E-Commerce)",
+    miniDesc:
+      "A web application that showcases a collection of products , allowing users to view, add, edit, and delete products. The application uses MongoDB, Express, Node.js, and EJS for rendering dynamic content.",
+    github: "https://github.com/Chetanshrivas/E-commerce",
+    img: "/assets/images/projects/shopping cart.webp.png",
+    seoimg: "/assets/images/projects/shoping cart.webp.png",
+    link: "E-commerce",
+    showOnHome: true,
+    tech: [
+      "JavaScript",
+      "Node.js",
+      "Express",
+      "MongoDB",
+      "EJS",
+      "Bootstrap",
+      "GSAP"
+    ],
+    desc: "A web application that showcases a collection of product, allowing users to view, add, edit, and delete products. The application uses MongoDB for data storage, Express and Node.js for the backend, and EJS for rendering dynamic content. Bootstrap and GSAP are used for responsive design and animations. This project demonstrates my full-stack development skills and ability to create interactive web applications."
+  },
+  {
+    title: "Brainwave",
+    miniDesc:
+      "Brainwave is a modern, responsive frontend web application built with Vite, React.js, and Tailwind CSS, showcasing cutting-edge UI/UX through sleek gradients, parallax animations, and component-based design best practices.",
+    website: "https://brainwave-chetan-shrivas-projects.vercel.app/",
+    github: "https://github.com/Chetanshrivas/Brainwave",
+    img: "/assets/images/projects/Brainwave.webp.png",
+    seoimg: "/assets/images/seo/Brainwave.webp.png",
+    link: "Brainwave-ai",
+    showOnHome: true,
+    tech: [
+      "Javascript",
+      "React.js",
+      "Tailwind CSS",
+      "Node.js",
+      "Vite",
+    ],
+    desc: "Brainwave is a modern, responsive frontend web application built with Vite, React.js, and Tailwind CSS, showcasing cutting-edge UI/UX through sleek gradients, parallax animations, and component-based design best practices."
+  },
+  // {
+  //   title: "News App",
+  //   miniDesc:
+  //     "A news application that fetches and displays the latest news articles from various sources. The app uses a news API and presents the information in an organized manner.",
+  //   github: "",
+  //   img: "/assets/images/projects/news-app.webp",
+  //   seoimg: "/assets/images/seo/news-app.webp",
+  //   link: "news-app",
+  //   showOnHome: false,
+  //   tech: [
+  //     "JavaScript",
+  //     "HTML",
+  //     "CSS",
+  //     "API",
+  //   ],
+  //   desc: "A news application that fetches and displays the latest news articles from various sources. The app uses a news API and presents the information in an organized manner. This project highlights my ability to work with APIs and build responsive web applications."
+  // },
+  {
+    title: "Weather App",
+    miniDesc:
+      "A web application that provides real-time weather information for any location. It uses a weather API to fetch data and displays it in a user-friendly interface.",
+    //website: "https://gauravsharma2003.github.io/My-Weather/",
+    github: "https://github.com/Chetanshrivas/Weather-App",
+    img: "/assets/images/projects/weather.webp.png",
+    seoimg: "/assets/images/seo/weather.webp.png",
+    link: "weather-app",
+    showOnHome: false,
+    tech: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+      "API",
+    ],
+    desc: "A web application that provides real-time weather information for any location. It uses a weather API to fetch data and displays it in a user-friendly interface. This project is developed using vanilla JavaScript, HTML, and CSS. It showcases my ability to work with APIs and create interactive web applications."
+  },
+  {
+    title: "Snake Game",
+    miniDesc:
+      "A classic snake game developed using JavaScript, HTML, and CSS. The game features simple controls and an engaging gameplay experience.",
+    github: "https://github.com/Chetanshrivas/Snake-game",
+    img: "/assets/images/projects/snakegame.webp.png",
+    seoimg: "/assets/images/seo/snakegame.webp.png",
+    link: "snake-game",
+    showOnHome: false,
+    tech: [
+      "JavaScript",
+      "HTML",
+      "CSS",
+    ],
+    desc: "A classic snake game developed using JavaScript, HTML, and CSS. The game features simple controls and an engaging gameplay experience. This project demonstrates my skills in game development and front-end technologies."
+  },
+  
+];
+
+export default projectData;
